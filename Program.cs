f8c8f8882daa1f833bab89973b57bd6dc0375655
Bruno Souza <bruno.arubesu@gmail.com>
2019-09-14 20:41:37 -0300

    add project files

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace prop_indexada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer[1] = new Customer("a", 1);
+            Console.WriteLine(customer[1]);
+        }
+    }
+
+    class Customer
+    {
+        private readonly IDictionary<int, Customer> customer = new Dictionary<int, Customer>();
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Customer(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public Customer()
+        {
+        }
+
+        public Customer this[int index]
+        {
+            get
+            {
+                return customer[index];
+            }
+            set
+            {
+                customer[index] = value;
+            }
+        }
+
+        public override string ToString() => $"{Name} {Id}";
+    }
+}
